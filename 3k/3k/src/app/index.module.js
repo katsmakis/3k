4504b4285895e96b1d9abcf66cbdf7f80c323096
antonisknks <antonisknks@gmail.com>
2017-02-18 21:08:12 +0200

    web app commit

---
+++ 3k/3k/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('3k', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
