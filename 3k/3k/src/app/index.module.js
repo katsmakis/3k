c2229ed20d8f685ce9483084ce8bb0b4c103469e
antonisknks <antonisknks@gmail.com>
2017-03-04 19:09:35 +0200

    del

--- 3k/3k/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('3k', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
