fc257e024c0dc7b81698a12e042b22cf32e713ac
maxalis <kakkavas.m@gmail.com>
2017-03-04 17:47:14 +0200

    Repos

---
+++ 3k/3k.Infrastructure/Repositories/AsthenisRepository.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _3k.Domain;
+using _3k.Domain.Interfaces.Repositories;
+
+namespace _3k.Infrastructure.Repositories
+{
+    public class AsthenisRepository : IAsthenistRepository
+    {
+        public AsthenisRepository()
+        {
+
+        }
+
+        public IEnumerable<Asthenis> GetAsthenisByEponimo(string partialEponimo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 3k/3k.Infrastructure/Services/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _3k.Domain.Interfaces.Services;
+using _3k.Domain;
+
+namespace _3k.Infrastructure.Services
+{
+    public class ClientService : IClientService
+    {
+        public ClientService()
+        {
+
+        }
+        public IEnumerable<Asthenis> GetClientByEponimo(string partialEponimo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
